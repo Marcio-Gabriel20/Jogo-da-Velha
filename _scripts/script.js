02662c66e3c392dbf53e4ce8a408bc19529eb66d
Marcio Gabriel <78151690+Marcio-Gabriel20@users.noreply.github.com>
2022-12-12 00:42:45 -0300

    Commit 5
    Criação da Lógica da condição de vitória.

--- _scripts/script.js
+++ _scripts/script.js
@@ -35,6 +35,10 @@ for(let i = 0; i < boxes.length; i++) {
                 player2++;
             }
 
+            // checar quem venceu
+
+            checkWinCondition();
+
         }
 
     });
@@ -54,5 +58,73 @@ function checkEl(player1, player2) {
     }
 
     return el;
+
+}
+
+// ver quem venceu
+
+function checkWin(b1, b2, b3) {
+        
+    if(b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0) {
+
+        let b1Child = b1.childNodes[0].className;
+        let b2Child = b2.childNodes[0].className;
+        let b3Child = b3.childNodes[0].className;
+
+        if(b1Child == 'x' && b2Child == 'x' && b3Child == 'x') {
+            // x
+            console.log('x venceu');
+        } else if(b1Child == 'o' && b2Child == 'o' && b3Child == 'o') {
+            console.log('o venceu');
+        }
+
+    }
+
+}
+
+function checkWinCondition() {
+
+    let b1 = document.getElementById('block-1');
+    let b2 = document.getElementById('block-2');
+    let b3 = document.getElementById('block-3');
+    let b4 = document.getElementById('block-4');
+    let b5 = document.getElementById('block-5');
+    let b6 = document.getElementById('block-6');
+    let b7 = document.getElementById('block-7');
+    let b8 = document.getElementById('block-8');
+    let b9 = document.getElementById('block-9');
+
+    // horizontal
+
+    checkWin(b1, b2, b3);
+    checkWin(b4, b5, b6);
+    checkWin(b7, b8, b9);
+
+    // vertical
+
+    checkWin(b1, b4, b7);
+    checkWin(b2, b5, b8);
+    checkWin(b3, b6, b9);
+
+    // diagonal
+
+    checkWin(b1, b5, b9);
+    checkWin(b3, b5, b7);
+
+    // deu velha
+
+    let counter = 0;
+
+    for(let i = 0; i < boxes.length; i++) {
+        
+        if(boxes[i].childNodes[0] != undefined) {
+            counter++;
+        }
+
+    }
+
+    if(counter == 9) {
+        console.log('deu velha');
+    }
 
 }
